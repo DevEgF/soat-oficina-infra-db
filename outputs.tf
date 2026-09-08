625632b8bc15c83d15c989db689013febae5e214
J Egito GS Filho <egitofilho4@gmail.com>
2026-09-07 22:45:09 -0300

    feat: add managed database alarms

--- outputs.tf
+++ outputs.tf
@@ -29,3 +29,12 @@ output "master_secret_arn" {
   value       = aws_db_instance.this.master_user_secret[0].secret_arn
   sensitive   = true
 }
+
+output "database_alarm_names" {
+  description = "CloudWatch alarms that protect the managed database."
+  value = [
+    aws_cloudwatch_metric_alarm.cpu.alarm_name,
+    aws_cloudwatch_metric_alarm.storage.alarm_name,
+    aws_cloudwatch_metric_alarm.connections.alarm_name,
+  ]
+}
